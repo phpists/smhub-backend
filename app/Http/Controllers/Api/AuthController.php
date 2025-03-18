d4ac555ef6550b7a1ce40a08e2f8d28d78daf458
Andrew Tsyupa <andy.tsyupa@gmail.com>
2025-03-18 19:58:14 +0200

    init

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'refresh', 'logout', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        if (!$token = Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function getUser()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'user' => auth()->user(),
+            'expires_in' => auth()->factory()->getTTL() * 60 * 24,
+            'token_type' => 'bearer',
+            'access_token' => $token,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('register', ['uses' => 'Api\AuthController@register']);
+    $router->post('login', ['uses' => 'Api\AuthController@login']);
+    $router->post('logout', ['uses' => 'Api\AuthController@logout']);
+
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->get('/user-profile', ['uses' => 'Api\AuthController@getUser']);
+    });
+});
