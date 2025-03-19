bfb17d23aeadd60966c5a5f623781c57dcf41a51
Urij <chopeyyuriy@gmail.com>
2025-03-19 10:42:40 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+$router->get('/', function () {
+    return app()->version();
+});
+
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('register', ['uses' => 'Api\AuthController@register']);
     $router->post('login', ['uses' => 'Api\AuthController@login']);
